8429feebeabf12cb2bb9c5539821ba036b566844
aranega <vincent.aranega@gmail.com>
2020-03-25 11:01:18 +0100

    Add first solution for param exercice

--- TD-refs_pointeurs/params.c
+++ TD-refs_pointeurs/params.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 
 
-int main(void)  // modifier la signature
+// Récupération des arguments sur la ligne de commandes
+int main(int argc, char **argv)
 {
+  printf("%d\n", argc);
 
-
+  for (int i = 0; i < argc; i++) {
+    // for (int j = 0; argv[i][j] != '\0'; j++) {
+    //   putchar(argv[i][j]);
+    // }
+    int j = 0;
+    while (argv[i][j] != '\0') putchar(argv[i][j++]);
+    putchar('\n');
+  }
   return 0;
 }
