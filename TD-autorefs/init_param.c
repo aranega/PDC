8fc9165bab1f150a7e822d20ab61e0648ade579c
aranega <vincent.aranega@gmail.com>
2020-04-01 08:59:40 +0200

    Add init params example/exercice

---
+++ TD-autorefs/init_param.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void init_int(/* ? */)
+{
+
+}
+
+
+void init_ptr(/* ? */)
+{
+
+}
+
+
+int main(void)
+{
+  int a;
+  int b;
+  int *pa;
+  /* ? */
+  return 0;
+}
