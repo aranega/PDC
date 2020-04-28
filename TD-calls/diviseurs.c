32fee457e7813419dd3abe19159672d46da65c68
aranega <vincent.aranega@gmail.com>
2020-04-28 12:39:17 +0200

    Add makefile and first diviseur file

---
+++ TD-calls/diviseurs.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+cell_t liste_des_diviseurs(unsigned int n)
+{
+  return NULL;
+}
+
+cell_t elements_communs(cell_t *a, cell_t *b)
+{
+	return NULL;
+}
+
+
+unsigned int plus_grand_element(cell_t *root)
+{
+  return 0;
+}
+
+unsigned int pgcd_inefficace(unsigned int a, unsigned int b)
+{
+  return 0;
+}
+
+int main(void)
+{
+  int res = pgcd_inefficace(15, 5);
+  printf("%u\n", res);
+  return 0;
+}
